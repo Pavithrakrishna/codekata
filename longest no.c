28e503490e3914717ca950875fe73b150beadd96
Pavithrakrishna <33005708+Pavithrakrishna@users.noreply.github.com>
2018-01-22 16:33:55 +0530

    Create longest no.c

---
+++ longest no.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int a=10,b=5,c=12;
+	if((a>b)&&(a>c))
+	printf("%d",a);
+	else
+	if((b>a)&&(b>c))
+	printf("%d",b);
+	else
+	printf("%d",c);
+	
+
+return 0;
+}
