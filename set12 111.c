378969887b2b8999fe82fb7c151710fba0a77511
Pavithrakrishna <33005708+Pavithrakrishna@users.noreply.github.com>
2018-03-07 16:58:00 +0000

    Create set12 111.c

---
+++ set12 111.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+  int num,n,i;
+  int count=0;
+  scanf("%d\n",&num);
+  n=num;
+  while(n!=0)
+  { count++; 
+     n=n/10;
+  }
+  printf("%d",count);
+   return 0;
+}
